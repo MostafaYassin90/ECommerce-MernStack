5e4b401f4c4de07cf0e83a0b8f2ab1c98609c125
MostafaYassin90 <mostafayassin292@gmail.com>
2025-01-16 00:47:15 +0200

    new commit

--- back-end/server.js
+++ back-end/server.js
@@ -11,7 +11,7 @@ import "dotenv/config";
 
 // App Config
 const app = express();
-const port = "4000";
+const port = 4000;
 
 // Connect DB
 connectDB();
@@ -26,9 +26,7 @@ app.use("/api/users", userRoute);
 app.use("/api/products", productRouter);
 app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
-app.use("/welcome", (req, res) => {
-  res.send("welcome");
-});
+
 
 // Running Server
 app.listen(port, () => {
